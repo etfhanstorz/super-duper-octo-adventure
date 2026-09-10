a4f51ada7690428a4ffb5d2a72248caa1b945c42
etfhanstorz <ethanstorz@gmail.com>
2026-09-10 12:18:56 -0600

    Created HungerGuard.java

---
+++ src/main/java/com/ethan/hungerguard/HungerGuard.java
@@ -0,0 +1,12 @@
+package com.ethan.hungerguard;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+
+@Mod(HungerGuard.MODID)
+public class HungerGuard {
+    public static final String MODID = "hungerguard";
+
+    public HungerGuard(IEventBus modEventBus) {
+    }
+}
